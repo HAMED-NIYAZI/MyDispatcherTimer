e1c9b6aa13b6a9305e4dd0a1648b764716cf736b
Hamed <USER@DESKTOP-20DRNS1>
2024-01-07 21:26:51 +0330

    add global

---
+++ MyDispatcherTimer/Global.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyDispatcherTimer
+{
+    public static class Global
+    {
+        public static int ID { get; set; }
+        public static int Name { get; set; }
+        public static List<ActInfo> ActInfos { get; set; }
+    }
+
+
+    public class ActInfo
+    {
+        public int Id { get; set; }
+        public DateTime LoginTime { get; set; }
+        public string NameAction { get; set; }
+
+    }
+}
